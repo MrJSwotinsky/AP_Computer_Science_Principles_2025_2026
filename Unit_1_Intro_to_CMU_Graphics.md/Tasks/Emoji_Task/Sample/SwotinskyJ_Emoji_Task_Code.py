e6a6e3403b80076be42b864452a660211b545a8a
MrJSwotinsky <jswotinsky@schools.nyc.gov>
2025-08-22 14:08:23 -0400

    Created initial image.
    Created face underwater wearing scuba mask.
    
    Next steps:
    -Add regulator and tube.

---
+++ Unit_1_Intro_to_CMU_Graphics.md/Tasks/Emoji_Task/Sample/SwotinskyJ_Emoji_Task_Code.py
@@ -0,0 +1,13 @@
+# Underwater Bacground:
+Rect(0,0,400,400, fill = 'lightSkyBlue')
+
+# Face (No eyes - Eyes included w/mask):
+Circle(200, 200,100, fill = 'yellow')
+Circle(200, 190, 15)
+Oval(200, 230, 100, 30)
+Rect(150, 215, 100, 15, fill = 'yellow')
+
+# Mask & Eyes:
+Rect(140,130,120,60, fill = 'lightYellow', border = 'blue')
+Circle(175, 160, 15)
+Circle(225, 160, 15)
